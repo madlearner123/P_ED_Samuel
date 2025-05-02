4543b7210e1e0862af863e407a7734c5c9070296
samuel <samuchase@gmail.com>
2025-05-02 13:54:36 +0200

    División implementada

--- Calculadora/src/modelos/Calculadora.java
+++ Calculadora/src/modelos/Calculadora.java
@@ -11,4 +11,8 @@ package modelos;
 public class Calculadora {
     
     public int sumar(int a, int b){return a + b;}
+    public int dividir(int a, int b){
+	if(b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
+	return a / b;
+    }
 }
